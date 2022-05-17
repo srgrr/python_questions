ea4988f7a3f70bdce3f6b2277df6cfd81b9fa0e0
Sergio Rodriguez <sergio.rodriguez@streamsets.com>
2022-05-17 14:59:28 +0200

    Add larger example for equal column problem

--- equal_columns.py
+++ equal_columns.py
@@ -9,3 +9,10 @@ def equal_columns(m):
 assert equal_columns([[1, 2, 3], [3, 2, 1]])
 assert not equal_columns([[1, 2], [2, 1]])
 assert not equal_columns([[]])
+assert not equal_columns(
+  [
+    [1, 3, 4, 5, 1],
+    [2, 4, 6, 2, 2],
+    [7, 2, 3, 2, 7]
+  ]
+)
